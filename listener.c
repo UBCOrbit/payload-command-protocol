ab77220cb79830f23000fe7fb9d44091996e84c5
Justin Frank <justinpfrank@protonmail.com>
2018-06-27 20:31:58 -0400

    changed verbose to a compile time option

--- listener.c
+++ listener.c
@@ -8,7 +8,7 @@
 
 #include "commands.h"
 
-#define VERBOSE true
+#define VERBOSE
 
 #define SERIAL_DEVICE "/dev/ttyTHS1"
 
@@ -27,8 +27,10 @@ void readAllOrDie(int fd, void *buf, size_t len)
     size_t offset = 0;
     ssize_t result;
     while (offset < len) {
-        if(VERBOSE) printf("    Waiting for data...\r");
+#ifdef VERBOSE
+        printf("    Waiting for data...\n");
         fflush(stdout);
+#endif
         result = read(fd, buf + offset, len - offset);
         if (result < 0) {
             perror("Error reading from file descriptor");
@@ -36,8 +38,10 @@ void readAllOrDie(int fd, void *buf, size_t len)
         }
         offset += result;
 
+#ifdef VERBOSE
         // Debug info
         printf("    Received: %ld out of %ld bytes.\n", offset, len);
+#endif
     }
 }
 
@@ -46,19 +50,30 @@ void writeAllOrDie(int fd, const void *buf, size_t len)
     size_t offset = 0;
     ssize_t result;
     while (offset < len) {
+#ifdef VERBOSE
+        printf("    Writing data...\n");
+        fflush(stdout);
+#endif
         result = write(fd, buf + offset, len - offset);
         if (result < 0) {
             perror("Error writing to file descriptor");
             exit(EXIT_FAILURE);
         }
         offset += result;
+
+#ifdef VERBOSE
+        printf("    Sent: %ld out of %ld bytes.\n", offset, len);
+#endif
     }
 }
 
 void writeMessage(int fd, const Message m)
 {
+#ifdef VERBOSE
     // Debug info
-    printf("Sending  reply:   %-19s with %8u bytes of data\n", reply_strs[m.code], m.payloadLen);
+    printf("\nSTART Message Write\n\n");
+    printf("    Sending reply: %-19s with %8u bytes of data\n", reply_strs[m.code], m.payloadLen);
+#endif
 
     uint8_t outHeader[3];
     outHeader[0] = m.code;
@@ -67,25 +82,35 @@ void writeMessage(int fd, const Message m)
     writeAllOrDie(fd, outHeader, sizeof(outHeader));
     if (m.payload != NULL)
         writeAllOrDie(fd, m.payload, m.payloadLen);
+
+#ifdef VERBOSE
+    printf("\nEND Message Write\n\n");
+#endif
 }
 
 Message readMessage(int fd)
 {
-    if (VERBOSE) printf("\nSTART Message Read\n\n");
-    if (VERBOSE) printf("    ---- HEADER ----\n");
+#ifdef VERBOSE
+    printf("\nSTART Message Read\n\n");
+    printf("    ---- HEADER ----\n");
+#endif
 
     uint8_t inHeader[3];
     readAllOrDie(fd, inHeader, sizeof(inHeader));
 
-    if (VERBOSE) printf("    Message header received.\n\n");
+#ifdef VERBOSE
+    printf("    Message header received.\n\n");
+#endif
 
     Message m;
     m.code = inHeader[0];
     memcpy(&m.payloadLen, inHeader + 1, 2);
 
-    if (VERBOSE) printf("    Command: %-19s. Payload length: %8u bytes.\n\n", command_strs[m.code], m.payloadLen);
+#ifdef VERBOSE
+    printf("    Command: %-19s Payload length: %8u bytes.\n\n", command_strs[m.code], m.payloadLen);
 
-    if (VERBOSE) printf("    ---- PAYLOAD ----\n");
+    printf("    ---- PAYLOAD ----\n");
+#endif
 
     m.payload = NULL;
     if (m.payloadLen > 0) {
@@ -93,12 +118,16 @@ Message readMessage(int fd)
         readAllOrDie(fd, m.payload, m.payloadLen);
     }
 
-    if (VERBOSE) printf("    Message payload received.\n\n");
+#ifdef VERBOSE
+    printf("    Message payload received.\n\n");
 
     // Debug info
-    if (VERBOSE) printf("    ---- SUMMARY ----\n");
+    printf("    ---- SUMMARY ----\n");
+#endif
     printf("    Received command: %-19s with %8u bytes of data\n", command_strs[m.code], m.payloadLen);
-    if (VERBOSE) printf("\nEND Message Read\n\n");
+#ifdef VERBOSE
+    printf("\nEND Message Read\n\n");
+#endif
 
     return m;
 }
@@ -106,14 +135,19 @@ Message readMessage(int fd)
 int main()
 {
     // Open the serial device
+#ifdef VERBOSE
     printf("Opening serial device...");
+#endif
     int serialfd = open(SERIAL_DEVICE, O_RDWR | O_NOCTTY | O_SYNC);
     if (serialfd < 0) {
         perror("Error opening serial device " SERIAL_DEVICE);
         // If the file descriptor failed to open, there's nothing we can do besides exit
         exit(EXIT_FAILURE);
     }
+
+#ifdef VERBOSE
     printf("Done.\n");
+#endif
 
     // Enter an infinite loop listening for and responding to messages
     while (true) {
